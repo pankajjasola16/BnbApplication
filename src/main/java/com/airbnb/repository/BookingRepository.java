b99b79edd9540a1a317b7ed751a2567842a65f91
Pankaj jasola <166109827+pankajjasola16@users.noreply.github.com>
2025-10-30 20:11:33 +0400

    Update BookingRepository.java
    with front end

--- src/main/java/com/airbnb/repository/BookingRepository.java
+++ src/main/java/com/airbnb/repository/BookingRepository.java
@@ -1,7 +1,11 @@
 package com.airbnb.repository;
 
+import com.airbnb.entity.AppUser;
 import com.airbnb.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByAppUser(AppUser user);
 }

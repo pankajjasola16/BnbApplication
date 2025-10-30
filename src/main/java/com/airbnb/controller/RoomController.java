8979eaac3b5cb16faeef5ca07cdfe47d399f84ad
Pankaj jasola <166109827+pankajjasola16@users.noreply.github.com>
2025-10-30 18:49:02 +0400

    Update RoomController.java
    with front end

--- src/main/java/com/airbnb/controller/RoomController.java
+++ src/main/java/com/airbnb/controller/RoomController.java
@@ -3,9 +3,9 @@ package com.airbnb.controller;
 
 import com.airbnb.entity.Room;
 import com.airbnb.repository.RoomRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 // GIVE PERMISSION TO THIS CONTROLLER IN --> SecurityConfig.java
 
@@ -24,4 +24,19 @@ public class RoomController {
 
         return "added";
     }
+
+    // below code is to get the type of the room, so that when I select the Hotel , its type will be autofill
+    // http://localhost:8080/rooms/byProperty/2
+    @GetMapping("/rooms/byProperty/{propertyId}")
+    public List<String> getRoomTypesByProperty(@PathVariable Long propertyId) {
+        return roomRepository.findByPropertyId(propertyId)
+                .stream()
+                .map(Room::getType)   // take only type
+                .distinct()           // avoid duplicates
+                .toList();
+    }
+
+
+
 }
+
